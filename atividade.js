8fb71d42753d400d36196d8b7af376485f68c9c2
gustahcl <126715826+gustahcl@users.noreply.github.com>
2024-09-18 13:54:52 +0000

    criação de prompt

---
+++ atividade.js
@@ -0,0 +1,14 @@
+var nome = " SeuNome ";
+var idade = " Sua idade ";
+console.log("Olá meu nome é"  + nome +  "e tenho"  + idade +  "anos.")
+
+
+var nome = " SeuNome ";
+var cidade = " SuaCidade ";
+console.log(nome + "é de" + cidade +'.');
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+console.log("soma:"+ soma, "subtracao:"+ subtracao, "multiplicacao:"+ multiplicacao, "divisao:"+ divisao);
---
+++ aula.js
@@ -0,0 +1,16 @@
+/*var soma = 10 + 85
+console.log(soma)
+
+myName ='Gustavo;'
+console.log(`Hello, ${myName}`)*/
+/*casa = string  numero = number*/
+
+
+
+
+var prompt = require('prompt-sync')();
+
+let nome = prompt("Qual é seu nome?");
+
+console.log(`Olá, ${nome}! Boas vindas a Faculdade Senac.`)
+
